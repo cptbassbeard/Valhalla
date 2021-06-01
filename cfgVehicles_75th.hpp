4d537f2ecb0b83becc3413a5f517b069d8f222b6
cullent <30484282+cullent@users.noreply.github.com>
2021-06-01 16:53:26 -0400

    test

--- cfgVehicles_75th.hpp
+++ cfgVehicles_75th.hpp
@@ -26,5 +26,12 @@ class cfgVehicles
 		displayName = "75th Ranger Regiment - Base Soldier";
 		modelSides[]={0,1,2,3};
 		faction="75th_RR"
-	}
+		backpack="";
+		items[] = {};
+		respawnItems[] = {};
+		linkedItems[] = {flb_CryeAVS_Assault01_GEAR_A_300_MCM, ItemCompass, ItemMap, ItemWatch, ACE_MapTools};
+		respawnlinkedItems[] = {flb_CryeAVS_Assault01_GEAR_A_300_MCM, ItemCompass, ItemMap, ItemWatch, ACE_MapTools};
+		weapons[] = {flb_M4A1_URGI_B5_13_FDE, throw, put};
+		respawnWeapon[] = {rhs_weap_mk18, throw, put};
+	};
 };
