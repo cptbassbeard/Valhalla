86158d478e098472c35afedca3373119361b7ea5
cullent <30484282+cullent@users.noreply.github.com>
2021-06-01 16:43:24 -0400

    updated 75th incremental

--- cfgVehicles_75th.hpp
+++ cfgVehicles_75th.hpp
@@ -15,5 +15,16 @@
 
 class cfgVehicles
 {
-	
-}
+	class g3_mc;
+	class 75th_RR_Soldier_base_f: g3_mc
+	{
+		author = "Team Valhalla";
+		scope = 1;
+		side = 1;
+		scopeCurator = 2;
+		scopeArsenal = 2;
+		displayName = "75th Ranger Regiment - Base Soldier";
+		modelSides[]={0,1,2,3};
+		faction="75th_RR"
+	}
+};
