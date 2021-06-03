00877283a8c1987c61512d5e57a5b904b51ba556
cullent <30484282+cullent@users.noreply.github.com>
2021-06-03 00:11:58 -0400

    fuck

--- 75th_RR/cfgPatches.hpp
+++ 75th_RR/cfgPatches.hpp
@@ -7,7 +7,7 @@ class CfgPatches
 		addonRootClass = "Valhalla";
 		units[]=
 		{
-			"75th_RR_Soldier_base_f", "75th_RR_Platoon_Leader", "75th_RR_Medic", "75th_RR_Platoon_Sgt", "75th_RR_JTAC", "75th_RR_Squad_Leader", "75th_RR_FTL", "75th_RR_Rifleman", "75th_RR_AR", "75th_RR_Grenadier", "75th_RR_MG", "75th_RR_AMG", "75th_RR_AT", "75th_RR_AAT"
+			"75thRR_Soldier_base_f"
 		};
 		requiredVersion=0.1;
 		requiredAddons[]={"A3_Characters_F"};
--- 75th_RR/cfgVehicles.hpp
+++ 75th_RR/cfgVehicles.hpp
@@ -3,19 +3,19 @@ class cfgVehicles
 {
 
 	class CUP_B_BAF_Soldier_AT_WDL;
-	class 75th_RR_Soldier_base_f: CUP_B_BAF_Soldier_AT_WDL
+	class 75thRR_Soldier_base_f: CUP_B_BAF_Soldier_AT_WDL
 	{
 		author = "Team Valhalla";
 		scope = 2;
 		side = 1;
 		scopeCurator = 2;
 		scopeArsenal = 2;
-		displayName = "75th RR - Base Soldier";
+		displayName = "75th_RR_Base_Soldier";
 		modelSides[]={0,1,2,3};
 		faction="75th_RR";
 		backpack="";
-		items[] = {};
-		respawnItems[] = {};
+		items[] = {IFAK};
+		respawnItems[] = {IFAK};
 		linkedItems[] = {flb_CryeAVS_Assault01_GEAR_A_300_MCM, ItemCompass, ItemMap, ItemWatch, ACE_MapTools};
 		respawnlinkedItems[] = {flb_CryeAVS_Assault01_GEAR_A_300_MCM, ItemCompass, ItemMap, ItemWatch, ACE_MapTools};
 		weapons[] = {flb_M4A1_URGI_B5_13_FDE, throw, put};
