a585221f70c4224ca73d504c7e06cd0721765387
cullent <30484282+cullent@users.noreply.github.com>
2021-06-03 18:05:45 -0400

    wtf

--- 75th_RR/config.cpp
+++ 75th_RR/config.cpp
@@ -2,5 +2,7 @@
 //Github repo VALHALLA
 	#include "cfgPatches.hpp"
 	#include "cfgFactionClasses.hpp"
+	#include "cfgWeapons.hpp"
 	#include "cfgVehicles.hpp"
+	#include "cfgUnits.hpp"
 	#include "cfgUnits.hpp"
