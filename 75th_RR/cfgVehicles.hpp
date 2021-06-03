190fd0317cdd84f68c959af886ea5b3a48a20c4c
cullent <30484282+cullent@users.noreply.github.com>
2021-06-03 18:50:32 -0400

    Finally unfucked this shit

--- 75th_RR/cfgVehicles.hpp
+++ 75th_RR/cfgVehicles.hpp
@@ -2,8 +2,8 @@
 class cfgVehicles
 {
 
-	class B_SOLDIER_SL_F;
-	class 75thRR_Soldier_base_f: B_SOLDIER_SL_F
+	class g3roll_mc_uniform;
+	class 75thRR_Soldier_base_f: g3roll_mc_uniform
 	{
 		author = "Team Valhalla";
 		scope = 2;
@@ -15,8 +15,8 @@ class cfgVehicles
 		backpack="";
 		items[] = {IFAK};
 		respawnItems[] = {IFAK};
-		linkedItems[] = {flb_CryeAVS_Assault01_GEAR_A_300_MCM, ItemCompass, ItemMap, ItemWatch, ACE_MapTools};
-		respawnlinkedItems[] = {flb_CryeAVS_Assault01_GEAR_A_300_MCM, ItemCompass, ItemMap, ItemWatch, ACE_MapTools};
+		linkedItems[] = {flb_CryeAVS_Assault01_GEAR_A_300_MCM, ItemCompass, ItemMap, ItemWatch, ACE_MapTools,FLB_PVS31_Tan_02,flb_opscover_peltor_C_03};
+		respawnlinkedItems[] = {flb_CryeAVS_Assault01_GEAR_A_300_MCM, ItemCompass, ItemMap, ItemWatch, ACE_MapTools,FLB_PVS31_Tan_02,flb_opscover_peltor_C_03};
 		weapons[] = {flb_M4A1_URGI_B5_13_FDE, throw, put};
 		respawnWeapon[] = {rhs_weap_mk18, throw, put};
 	};
