b238eebef0e092f553cfdc5ae2bf939de1e71834
cullent <30484282+cullent@users.noreply.github.com>
2021-06-07 21:04:59 -0400

    poop sandal

--- 75th_RR/cfgWeapons.hpp
+++ 75th_RR/cfgWeapons.hpp
@@ -1,8 +1,18 @@
 class cfgWeapons
 {
+	class WeaponSlotInfo;
+	class MuzzleSlot;
 	class flb_M4A1_URGI_B5_13_MIX;
 	class Ranger_M4_BLK3: flb_M4A1_URGI_B5_13_MIX
 	{
+		author="Team Valhalla";
+		baseWeapon="flb_M4A1_URGI_B5_13_MIX";
+		displayName= "Ranger M4A1 Block 3"
+		class MuzzleSlot: MuzzleSlot {
+			compatibleItems[] = {
+				"FLB_556_cordurawrap_tan_v2";
+			};
+		};
 		scope = 2;
 		class LinkedItems
 		{
