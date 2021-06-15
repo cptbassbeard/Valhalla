4f780f8c20dee70045eabd84bb7feea9eeba80a3
cullent <30484282+cullent@users.noreply.github.com>
2021-06-15 19:37:31 -0400

    75th Good Progress build

--- 75th_RR/cfgVehicles.hpp
+++ 75th_RR/cfgVehicles.hpp
@@ -5,4 +5,8 @@ class cfgVehicles
 
 	RANGER_SOLDIER(75th_RF_MC,"75th Ranger Rifleman MC",g3roll_mc, flb_CryeAVS_Assault01_GEAR_A_300_MCM,flb_opscover_peltor_C_03,RF_Items,RF_Linked,RF_WEP,RF_MAG,flb_mappack_Standard_mc);
 
+	RANGER_SOLDIER(75th_RF_MC,"75th Ranger Rifleman MC",g3roll_mc, flb_CryeAVS_Assault01_GEAR_A_300_MCM,flb_opscover_peltor_C_03,RF_Items,RF_Linked,RF_WEP,RF_MAG,flb_mappack_Standard_mc);
+
+	RANGER_SOLDIER(75th_RF_MC,"75th Ranger Rifleman MC",g3roll_mc, flb_CryeAVS_Assault01_GEAR_A_300_MCM,flb_opscover_peltor_C_03,RF_Items,RF_Linked,RF_WEP,RF_MAG,flb_mappack_Standard_mc);
+
 };
