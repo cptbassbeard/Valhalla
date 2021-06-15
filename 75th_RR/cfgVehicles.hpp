126d35598676298e26ca5e43bdfd9b5e69c61d67
cullent <30484282+cullent@users.noreply.github.com>
2021-06-14 23:19:57 -0400

    Macro Test MK4

--- 75th_RR/cfgVehicles.hpp
+++ 75th_RR/cfgVehicles.hpp
@@ -18,7 +18,7 @@ class cfgVehicles
 		linkedItems[] = {flb_CryeAVS_Assault01_GEAR_A_300_MCM, ItemCompass, ItemMap, ItemWatch,FLB_PVS31_Tan_02,flb_opscover_peltor_C_03};
 		respawnlinkedItems[] = {flb_CryeAVS_Assault01_GEAR_A_300_MCM, ItemCompass, ItemMap, ItemWatch, FLB_PVS31_Tan_02,flb_opscover_peltor_C_03};
 		weapons[] = {Ranger_M2010, Ranger_M17, throw, put};
-		respawnWeapon[] = {Ranger_M2010, Ranger_M17, throw, put};
+		respawnWeapons[] = {Ranger_M2010, Ranger_M17, throw, put};
 	};
 
 	75th_Soldier(75th_RF_MC,"75th Ranger Rifleman MC",g3roll_mc,RF_Items,RF_Linked,RF_WEP,flb_mappack_Standard_mc);
