330630a56249b71beb98c7b316c3bfdbbf4bad43
cptbassbeard <54035056+cptbassbeard@users.noreply.github.com>
2021-08-18 14:32:48 +0100

    Adds zeus enhanced as a dependancy for module hook (Safe start + endex + fpscheck-BETA)

--- config.cpp
+++ config.cpp
@@ -11,7 +11,7 @@ class CfgPatches
 		requiredVersion = 1.60; 
 		// Required addons, used for setting load order.
 		// When any of the addons is missing, pop-up warning will appear when launching the game.
-		requiredAddons[] = { "A3_Functions_F" };
+		requiredAddons[] = { "A3_Functions_F", "zen_context_menu","zen_attributes" };
 		// List of objects (CfgVehicles classes) contained in the addon. Important also for Zeus content (units and groups) unlocking.
 		units[] = {};
 		// List of weapons (CfgWeapons classes) contained in the addon.
